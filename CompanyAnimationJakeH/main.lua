3f33437c2237bdc41599eeda59f0bb924b3ce804
Jake Holtzhauer <47784147+yungprod@users.noreply.github.com>
2019-04-10 09:30:46 -0400

    finished company animation

--- CompanyAnimationJakeH/main.lua
+++ CompanyAnimationJakeH/main.lua
@@ -1,7 +1,28 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+--Jake Holtzhauer
+--Animating Images
+--this project animates our groups company logo
 
--- Your code here
+display.setStatusBar(display.HiddenStatusBar)
+
+local CompanyLogo = display.newImageRect("Images/CompanyLogoJakeH@2x - copy.png", 500, 500 )
+local AnimationSound =audio.loadSound( "Sounds/wowc.mp3" )
+local AnimationSoundChannel
+
+--global variables
+scrollspeed = 3
+
+--creating the company logo
+CompanyLogo.x = 0 
+CompanyLogo.y = display.contentHeight
+
+--rotating the logo (and transitioning the logo)
+transition.to( CompanyLogo, { rotation = CompanyLogo.rotation-360, time=4000, onComplete=spinImage})
+transition.to(CompanyLogo, {x= 512, y=384, time=4000})
+
+--Plays the animation sound
+AnimationSoundChannel = audio.play(AnimationSound)
+
+
+
+
+
